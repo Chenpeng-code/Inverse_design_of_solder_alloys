f46261426143d468ea81deb1304bd843fe7346da
Chenpeng <770597408@qq.com>
2026-04-14 21:35:58 +0800

    cpu

--- Inverse_design_of_amorphous_alloys-main/模拟/vae_gen_2_随机生成成分数据.py
+++ Inverse_design_of_amorphous_alloys-main/模拟/vae_gen_2_随机生成成分数据.py
@@ -29,8 +29,8 @@ class Encoder(nn.Module):
         self.linear3 = nn.Linear(32, latent_dims)
 
         self.N = torch.distributions.Normal(0, 1)
-        self.N.loc = self.N.loc.cuda() # hack to get sampling on the GPU
-        self.N.scale = self.N.scale.cuda()
+        self.N.loc = self.N.loc # hack to get sampling on the GPU
+        self.N.scale = self.N.scale
         self.kl = 0
 
 
